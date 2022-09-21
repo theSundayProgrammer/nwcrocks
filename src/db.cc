a8bbb14f54f1633321d678a91ecb4feb1d257f2e
Joe Mariadassou <theSundayProgrammer@gmail.com>
2022-09-21 19:10:09 +1000

    incorporating write batch

--- src/db.cc
+++ src/db.cc
@@ -14,19 +14,21 @@ using ROCKSDB_NAMESPACE::Status;
 using ROCKSDB_NAMESPACE::WriteBatch;
 using ROCKSDB_NAMESPACE::WriteOptions;
 using ROCKSDB_NAMESPACE::ColumnFamilyHandle;
-const char* table="nwc_db";
  ROCKSDB_NAMESPACE::Options options_from_table(lua_State *L, int index) ;
     int open_db(lua_State *L);
     int open_cf(lua_State *L);
     namespace {
+        const char* table="nwc_db";
         int get(lua_State* L) ;
         int put(lua_State* L) ;
         int close(lua_State *L);
+        int batch_begin(lua_State *L);
         int remove(lua_State *L);
         const struct luaL_Reg  db_reg[] = {
             { "get", get },
             { "put", put },
             { "remove", remove },
+            { "batch_begin", batch_begin },
             { "close", close },
             { NULL, NULL }
         };
@@ -39,18 +41,21 @@ const char* table="nwc_db";
 
         struct rocks_db {
             DB* db=nullptr;
-            bool open=false;
             Options options;
+            std::string path;
             ~rocks_db () {
                 delete db;
+                db=nullptr;
             }
         };
 
         int close(lua_State *L) {
             int index = 1;
+            //fprintf(stderr,"%s=%d\n", __FILE__,__LINE__);
             rocks_db *d = (rocks_db*) luaL_checkudata(L, index, table);
-            luaL_argcheck(L, d != NULL && d->db != NULL, index, "db expected");
-            d->~rocks_db(); ;
+            luaL_argcheck(L, d != NULL , index, "db expected");
+            if(d->db)
+                d->~rocks_db(); ;
 
             return 1;
         }
@@ -68,11 +73,18 @@ const char* table="nwc_db";
             }
             return 1;
         }
+        int batch_begin(lua_State* L) {
+            int argc=0;
+            rocks_db *d = (rocks_db*) luaL_checkudata(L, ++argc, table);
+            lrocks::writebatch_create(L, d->db);
+            return 1;
+        }
         int put(lua_State* L) {
             using ROCKSDB_NAMESPACE::Slice;
             using lrocks::get_str;
             int argc=0;
             rocks_db *d = (rocks_db*) luaL_checkudata(L, ++argc, table);
+            //fprintf(stderr,"path=%s\n", d->path.c_str());
             std::string key = get_str(L, ++argc);
             std::string value = get_str(L, ++argc);
             Status s = d->db->Put(WriteOptions(), Slice(key), Slice(value));
@@ -104,45 +116,46 @@ const char* table="nwc_db";
             return 1;
         }
     }
- int open_db(lua_State *L) {
-     DB* db;
+int open_db(lua_State *L) {
+    DB* db;
     static bool init = [=]() {
-    lrocks::createmeta(L, table, db_reg);
-    return true;
+        lrocks::createmeta(L, table, db_reg);
+        return true;
     }();
-     int argc = 0;
-     Options options = options_from_table(L, ++argc);
-     // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
-     options.IncreaseParallelism();
-     options.OptimizeLevelStyleCompaction();
-     // create the DB if it's not already present
+    int argc = 0;
+    Options options = options_from_table(L, ++argc);
+    // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+    options.IncreaseParallelism();
+    options.OptimizeLevelStyleCompaction();
 
-     //fprintf(stderr," create = %s\n", options.create_if_missing ? "true": "false");
-     //fprintf(stderr," create = %s\n", options.create_missing_column_families? "true": "false");
-     // open DB
+    //fprintf(stderr," create = %s\n", options.create_if_missing ? "true": "false");
+    //fprintf(stderr," create = %s\n", options.create_missing_column_families? "true": "false");
+    // open DB
 
-     const char *path = luaL_checkstring(L, ++argc);
+    std::string path = lrocks::get_str(L, ++argc);
 
-     Status s = DB::Open(options, path, &db);
-     //assert(s.ok());
-     if(!s.ok()) {
+    Status s = DB::Open(options, path, &db);
+    //assert(s.ok());
+    if(!s.ok()) {
         fprintf(stderr,"status = %s\n", s.getState());
-         luaL_error(L, "failed to open");
-         return 0;
-     }
-     rocks_db *d = new (lua_newuserdata(L, sizeof(rocks_db))) rocks_db();
-     d->options=options;
-     d->db = db;
-     d->open = true;
-     lrocks::setmeta(L, table);
+        luaL_error(L, "failed to open");
+        return 0;
+    }
+    rocks_db *d = new (lua_newuserdata(L, sizeof(rocks_db))) rocks_db();
+    d->options=options;
+    d->db = db;
+    path.swap(d->path);
+    //lrocks::setmeta(L, table);
+    luaL_getmetatable(L, table);
+    lua_setmetatable(L, -2);
 
-     return 1;
- }
+    return 1;
+}
 
 
 
 
-#ifdef __cplusplus
+#ifdef __cplusplus 
 extern "C"
 #endif
 DLL_PUBLIC int luaopen_nwcrocks(lua_State *L) {
