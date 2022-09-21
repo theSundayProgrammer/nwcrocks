be7b0a4bb4a2252e252e296255f40d8d999b770d
Joe Mariadassou <theSundayProgrammer@gmail.com>
2022-09-21 19:02:32 +1000

    Adding writebatch to make

--- include/db.h
+++ include/db.h
@@ -1,16 +1,19 @@
 #pragma once
 #define DLL_PUBLIC __attribute__ ((visibility ("default")))
-extern "C" {
-#include <luajit/lauxlib.h>
-}
+#include <luajit/lua.hpp>
 #include <string>
+namespace rocksdb{
+    class DB;
+}
 namespace lrocks {
     int createmeta(lua_State *L, const char *name, const luaL_Reg *methods) ;
     void setfuncs (lua_State *L, const luaL_Reg *l) ;
     void setmeta(lua_State *L, const char *name) ;
     void assert(lua_State *L, int cond, const char *msg) ;
     std::string get_str(lua_State* L, int index);
+    int writebatch_create(lua_State *L, rocksdb::DB*);
 }
+
 #include "rocksdb/db.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/options.h"
